9ceeb34872e50c8c3cdea6a0efe382f3975f0708
Creapermann <prtnprvtmail@protonmail.com>
2022-04-22 00:32:40 +0200

    Error handling middleware added

---
+++ book_app_learning/src/Application/Common/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,45 @@
+using System.Net;
+using Application.Common.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+
+namespace Application.Common.Middleware
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ErrorHandlingMiddleware> _logger;
+        private readonly IHostEnvironment _env;
+
+        public ErrorHandlingMiddleware(RequestDelegate next, ILogger<ErrorHandlingMiddleware> logger, IHostEnvironment env)
+        {
+            _next = next;
+            _logger = logger;
+            _env = env;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                var response = _env.IsDevelopment() ?
+                                new ApiErrorDto(context.Response.StatusCode, ex.Message, ex.StackTrace)
+                                : new ApiErrorDto(context.Response.StatusCode, ex.Message);
+
+
+                await context.Response.WriteAsync(response.ToString());
+            }
+        }
+    }
+}
